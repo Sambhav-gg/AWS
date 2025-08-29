ed76dfa9cf1cdafdcc848aec596ea388c3cd1d50
Sambhav-gg <156508582+Sambhav-gg@users.noreply.github.com>
2025-08-29 17:00:10 +0530

    Implemented user

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       next: (data) => setTodos([...data.items]),
     });
   }, []);
-  const { signOut } = useAuthenticator();
+    const { user, signOut } = useAuthenticator();
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
@@ -25,7 +25,8 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+            <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      {/* <h1>My todos</h1> */}
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -37,9 +38,9 @@ function App() {
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+        {/* <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
-        </a>
+        </a> */}
       </div>
             <button onClick={signOut}>Sign out</button>
     </main>
